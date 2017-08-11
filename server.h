9e0be962459728ff44f4a04498170894b70bfff3
MMolieleng <mmoliele@e4r4p2.wtc.co.za>
2017-08-11 11:40:32 +0200

    Server working perfectly

--- server.h
+++ server.h
@@ -6,8 +6,27 @@
 /*   By: mmoliele <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/11 09:54:42 by mmoliele          #+#    #+#             */
-/*   Updated: 2017/08/11 09:54:44 by mmoliele         ###   ########.fr       */
+/*   Updated: 2017/08/11 10:23:51 by mmoliele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SERVER_H
+# define SERVER_H
 
+# include <stdio.h>
+# include <string.h> 
+# include <stdlib.h> 
+# include <errno.h> 
+# include <unistd.h> 
+# include <arpa/inet.h> 
+# include <sys/types.h> 
+# include <sys/socket.h> 
+# include <netinet/in.h> 
+# include <sys/time.h>
+
+#define TRUE 1
+#define FALSE 0
+#define PORT 8888
+
+void	init();
+#endif
